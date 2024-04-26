e66eed06718ab613bcbd78909a43def07f815453
Aung Phyo Thant <aungphyothant224@gmail.com>
2024-04-26 10:40:47 +0630

    - Callbacks.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 console.log("Before");
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+    console.log(user);
+
+    getRepositories(user.gitHubUsername, (repo) => {
+        console.log(repo);
+    });
+});
 console.log("After");
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
         console.log("Reading a user form the database....");
-        return { id: id, gitHubUsername: 'mosh' };
+        callback({ id: id, gitHubUsername: 'mosh' });
     }, 2000);
+}
 
-    return 1;
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log("Calling GitHub API....");
+        callback({ name: username, repositories: ['repo1', 'repo2', 'repo3'] });
+    }, 2000);
 }
