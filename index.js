9f4d70c2c5038468db7e48dbcce6acfdb4d1072b
Aung Phyo Thant <aungphyothant224@gmail.com>
2024-04-26 09:59:52 +0630

    - Patterns for Dealing with Asynchronous Code.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 console.log("Before");
-setTimeout(() => {
-    console.log("Reading a user form the database....");
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log("After");
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log("Reading a user form the database....");
+        return { id: id, gitHubUsername: 'mosh' };
+    }, 2000);
+
+    return 1;
+}
